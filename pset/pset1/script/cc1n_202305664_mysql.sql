82c194f9f6e735b1f2d48aa4764b6dc2ec16764f
Erick Jun Kato <116921226+nomenome-cmd@users.noreply.github.com>
2023-05-26 23:32:19 -0300

    mysql.sql

--- pset/pset1/script/cc1n_202305664_mysql.sql
+++ pset/pset1/script/cc1n_202305664_mysql.sql
@@ -15,15 +15,15 @@ USE             uvv;
 
 -- Criação da tabela com o nome lojas.produtos.
 CREATE TABLE 	produtos (
-    			produto_id                  NUMERIC(38) 	NOT NULL,
-    			nome                        VARCHAR(255) 	NOT NULL,
-    			preco_unitario              NUMERIC(10,2),
-    			detalhes                    BLOB,
-    			imagem                      BLOB,
-    			imagem_mime_type            VARCHAR(512),
-    			imagem_arquivo              VARCHAR(512),
-    			imagem_charset              VARCHAR(512),
-    			imagem_ultima_atualizacao   DATE,
+                produto_id                  NUMERIC(38) 	NOT NULL,
+                nome                        VARCHAR(255) 	NOT NULL,
+                preco_unitario              NUMERIC(10,2),
+                detalhes                    BLOB,
+                imagem                      BLOB,
+                imagem_mime_type            VARCHAR(512),
+                imagem_arquivo              VARCHAR(512),
+                imagem_charset              VARCHAR(512),
+                imagem_ultima_atualizacao   DATE,
 CONSTRAINT      pk_produtos                 PRIMARY KEY  (produto_id)
 );
 
@@ -33,9 +33,9 @@ ADD CHECK       (preco_unitario > 0);
 
 -- Comentários para explicações de tabela e colunas de tabela lojas.produto.
 ALTER TABLE     produtos                    COMMENT 'A tabela produtos que tem por sua responsabilidade de guardar os dados dos produtos.';
-ALTER TABLE     produtos                    MODIFY COLUMN   produto_id 		    NUMERIC(38)     COMMENT 'Coluna produto_id que é responsável como identificador único da tabela produtos. Ele também é uma chave primária da tabela produtos.';
-ALTER TABLE     produtos                    MODIFY COLUMN   nome 				VARCHAR(255)    COMMENT 'Coluna nome que é responsável por guardar o nome dos produtos.';
-ALTER TABLE     produtos                    MODIFY COLUMN   preco_unitario 	    NUMERIC(10,2)   COMMENT 'Coluna preco_unitario que é responsável por mostrar o preço de unidade por cada produto.';
+ALTER TABLE     produtos                    MODIFY COLUMN   produto_id          NUMERIC(38)     COMMENT 'Coluna produto_id que é responsável como identificador único da tabela produtos. Ele também é uma chave primária da tabela produtos.';
+ALTER TABLE     produtos                    MODIFY COLUMN   nome                VARCHAR(255)    COMMENT 'Coluna nome que é responsável por guardar o nome dos produtos.';
+ALTER TABLE     produtos                    MODIFY COLUMN   preco_unitario      NUMERIC(10,2)   COMMENT 'Coluna preco_unitario que é responsável por mostrar o preço de unidade por cada produto.';
 ALTER TABLE     produtos                    MODIFY COLUMN   detalhes            BLOB            COMMENT 'Coluna detalhes que é responsável para detalhar objeto de tipo de arquivo como forma de armazenamento dos produtos.';
 ALTER TABLE     produtos                    MODIFY COLUMN   imagem              BLOB            COMMENT 'Coluna imagem que é responsável por armazenar a imagem do produto.';
 ALTER TABLE     produtos                    MODIFY COLUMN   imagem_mime_type    VARCHAR(512)    COMMENT 'Coluna loja que é responsável por armazenar o tipo de arquivo da imagem do produto.';
@@ -45,9 +45,9 @@ ALTER TABLE     produtos                    MODIFY COLUMN   imagem_ultima_atuali
 
 -- Criação de tabela com o nome de lojas.
 CREATE TABLE 	lojas (
-    			loja_id                     NUMERIC(38)     NOT NULL,
-   				nome                        VARCHAR(255)    NOT NULL,
-    			endereco_web                VARCHAR(200),
+                loja_id                     NUMERIC(38)     NOT NULL,
+                nome                        VARCHAR(255)    NOT NULL,
+                endereco_web                VARCHAR(200),
                 endereco_fisico         	VARCHAR(512),
                 latitude                	NUMERIC,
                 longitude               	NUMERIC,
@@ -76,14 +76,14 @@ ALTER TABLE     lojas                       MODIFY COLUMN   logo                
 ALTER TABLE     lojas                       MODIFY COLUMN   logo_mime_type      VARCHAR(512)    COMMENT 'Coluna logo_mime_type que é responsável por armazenar o tipo de arquivo da imagem de um logo da loja.';
 ALTER TABLE     lojas                       MODIFY COLUMN   logo_arquivo        VARCHAR(512)    COMMENT 'Coluna logo_arquivo que é responsável por armazenar o arquivo da imagem de um logo da loja.';
 ALTER TABLE     lojas                       MODIFY COLUMN   logo_charset        VARCHAR(512)    COMMENT 'Coluna logo_charset que é responsável por armazenar codificação dos caracteres da imagem de um logo da loja.';
-ALTER TABLE     lojas                       MODIFY COLUMN   logo_ultima_atualizacao DATE            COMMENT 'Coluna logo_ultima_atualizacao que é responsável por armazenar a última atualização de um logo da loja.';
+ALTER TABLE     lojas                       MODIFY COLUMN   logo_ultima_atualizacao     DATE    COMMENT 'Coluna logo_ultima_atualizacao que é responsável por armazenar a última atualização de um logo da loja.';
 
 -- Criação de tabela com nome de estoques.
 CREATE TABLE 	estoques (
-    			estoque_id 		            NUMERIC(38) 	NOT NULL,
-    			loja_id 		            NUMERIC(38) 	NOT NULL,
-    			produto_id 		            NUMERIC(38) 	NOT NULL,
-    			quantidade 		            NUMERIC(38) 	NOT NULL,
+                estoque_id 		            NUMERIC(38) 	NOT NULL,
+                loja_id 		            NUMERIC(38) 	NOT NULL,
+                produto_id 		            NUMERIC(38) 	NOT NULL,
+                quantidade 		            NUMERIC(38) 	NOT NULL,
 CONSTRAINT      pk_estoques                 PRIMARY KEY (estoque_id)
 );
 
@@ -101,51 +101,51 @@ ALTER TABLE     estoques                    MODIFY COLUMN   quantidade          
 
 -- Criação de tabela com o nome de lojas.clientes.
 CREATE TABLE    clientes (
-                cliente_id 		            NUMERIC(38)		NOT NULL,
-                email 			            VARCHAR(255) 	NOT NULL,
-                nome 				        VARCHAR(255) 	NOT NULL,
-                telefone1 		            VARCHAR(20),
-                telefone2 		            VARCHAR(20),
-                telefone3 		            VARCHAR(20),
+                cliente_id                  NUMERIC(38)		NOT NULL,
+                email                       VARCHAR(255) 	NOT NULL,
+                nome                        VARCHAR(255) 	NOT NULL,
+                telefone1                   VARCHAR(20),
+                telefone2                   VARCHAR(20),
+                telefone3                   VARCHAR(20),
 CONSTRAINT      pk_clientes                 PRIMARY KEY (cliente_id)
 );
 
 -- Comentários para explicações de tabela e colunas de tabela lojas.clientes.
 ALTER TABLE     clientes                    COMMENT 'A tabela Clientes que tem por sua responsabilidade de guardar os dados dos seus clientes.';
 ALTER TABLE     clientes                    MODIFY COLUMN   cliente_id 		    NUMERIC(38)  	COMMENT 'Coluna cliente_id que é responsável como identificador único da tabela clientes. Ele também é uma chave primária da tabela clientes.';
-ALTER TABLE     clientes                    MODIFY COLUMN   email 			    VARCHAR(255)   	COMMENT 'Coluna email que é responsável por guardar o email do cliente.';
-ALTER TABLE     clientes                    MODIFY COLUMN   nome 			    VARCHAR(255)   	COMMENT 'Coluna nome que é responsável por guardar o nome do cliente.';
+ALTER TABLE     clientes                    MODIFY COLUMN   email               VARCHAR(255)   	COMMENT 'Coluna email que é responsável por guardar o email do cliente.';
+ALTER TABLE     clientes                    MODIFY COLUMN   nome                VARCHAR(255)   	COMMENT 'Coluna nome que é responsável por guardar o nome do cliente.';
 ALTER TABLE     clientes                    MODIFY COLUMN   telefone1 		    VARCHAR(20) 	COMMENT 'Coluna telefone1 que é responsável por guardar o número de telefone do cliente.';
 ALTER TABLE     clientes                    MODIFY COLUMN   telefone2 		    VARCHAR(20) 	COMMENT 'Coluna telefone2 que é responsável por guardar o segundo número de telefone do cliente.';
 ALTER TABLE     clientes                    MODIFY COLUMN   telefone3 		    VARCHAR(20) 	COMMENT 'Coluna telefone3 que é responsável por guardar o terceiro número de telefone do cliente.';
 
 -- Criação de tabela com o nome de lojas.envios.
 CREATE TABLE    envios (
-                envio_id 		            NUMERIC (38) 	NOT NULL,
-                loja_id 		            NUMERIC (38) 	NOT NULL,
-                cliente_id 	                NUMERIC (38)	NOT NULL,
-                endereco_entrega 	        VARCHAR(512)    NOT NULL,
-                status 			            VARCHAR(15)     NOT NULL,
+                envio_id                    NUMERIC (38) 	NOT NULL,
+                loja_id                     NUMERIC (38) 	NOT NULL,
+                cliente_id                  NUMERIC (38)	NOT NULL,
+                endereco_entrega            VARCHAR(512)    NOT NULL,
+                status                      VARCHAR(15)     NOT NULL,
 CONSTRAINT      pk_envios                   PRIMARY KEY (envio_id)
 );
 
 -- Comentários para explicações de tabela e colunas de tabela lojas.envios.
 ALTER TABLE     envios                      COMMENT 'A tabela envios que tem por sua responsabilidade de guardar os dados dos envios realizados.';
-ALTER TABLE     envios                      MODIFY COLUMN   envio_id 			NUMERIC(38)  	COMMENT 'Coluna envio_id que é responsável como identificador único da tabela envios. Ele também é uma chave primária da tabela envios.';
-ALTER TABLE     envios                      MODIFY COLUMN   loja_id 			NUMERIC(38)  	COMMENT 'Coluna loja_id que é responsável como identificador da tabela envios. Ele também é uma chave estrangeira da tabela lojas.';
+ALTER TABLE     envios                      MODIFY COLUMN   envio_id            NUMERIC(38)  	COMMENT 'Coluna envio_id que é responsável como identificador único da tabela envios. Ele também é uma chave primária da tabela envios.';
+ALTER TABLE     envios                      MODIFY COLUMN   loja_id             NUMERIC(38)  	COMMENT 'Coluna loja_id que é responsável como identificador da tabela envios. Ele também é uma chave estrangeira da tabela lojas.';
 ALTER TABLE     envios                      MODIFY COLUMN   cliente_id 		    NUMERIC(38) 	COMMENT 'Coluna cliente_id que é responsável como identificador da tabela envios. Ele também é uma chave estrangeira da tabela clientes.';
-ALTER TABLE     envios                      MODIFY COLUMN   endereco_entrega 	VARCHAR(512)  	COMMENT 'Coluna endereco_entrega que é responsável por guardar o endereço de entrega do envio.';
-ALTER TABLE     envios                      MODIFY COLUMN   status 			    VARCHAR(15) 	COMMENT 'Coluna status que é responsável por guardar o status do envio.';
+ALTER TABLE     envios                      MODIFY COLUMN   endereco_entrega    VARCHAR(512)  	COMMENT 'Coluna endereco_entrega que é responsável por guardar o endereço de entrega do envio.';
+ALTER TABLE     envios                      MODIFY COLUMN   status              VARCHAR(15) 	COMMENT 'Coluna status que é responsável por guardar o status do envio.';
 
 
 
 -- Criação de tabela com o nome de lojas.pedidos.
 CREATE TABLE    pedidos (
-                pedido_id 			        NUMERIC (38) 	NOT NULL,
-                data_hora 			        TIMESTAMP 		NOT NULL,
-                cliente_id 			        NUMERIC (38)	NOT NULL,
-                status 				        VARCHAR (15) 	NOT NULL,
-                loja_id 				    NUMERIC (38) 	NOT NULL,
+                pedido_id                   NUMERIC (38) 	NOT NULL,
+                data_hora                   TIMESTAMP       NOT NULL,
+                cliente_id                  NUMERIC (38)	NOT NULL,
+                status                      VARCHAR (15)    NOT NULL,
+                loja_id                     NUMERIC (38) 	NOT NULL,
 CONSTRAINT      pk_pedidos                  PRIMARY KEY (pedido_id)
 );
 
@@ -153,26 +153,26 @@ CONSTRAINT      pk_pedidos                  PRIMARY KEY (pedido_id)
 ALTER TABLE     pedidos                     COMMENT 'A tabela pedidos que tem por sua responsabilidade de guardar pedidos dos seus clientes à loja.';
 ALTER TABLE     pedidos                     MODIFY COLUMN   pedido_id 	        NUMERIC(38)    	COMMENT 'Coluna pedido_id que é responsável como identificador único da tabela pedidos. Ele também é uma chave primária da tabela pedidos.';
 ALTER TABLE     pedidos                     MODIFY COLUMN   cliente_id 	        NUMERIC(38)   	COMMENT 'Coluna cliente_id que é responsável como identificador da tabela pedidos. Ele também é uma chave estrangeira da tabela clientes.';
-ALTER TABLE     pedidos                     MODIFY COLUMN   status 	            VARCHAR(15)   	COMMENT 'Coluna status que é responsável por mostrar o estado atual dos pedidos.';
+ALTER TABLE     pedidos                     MODIFY COLUMN   status              VARCHAR(15)   	COMMENT 'Coluna status que é responsável por mostrar o estado atual dos pedidos.';
 ALTER TABLE     pedidos                     MODIFY COLUMN   loja_id 	        NUMERIC(38)   	COMMENT 'Coluna loja_id que é responsável como identificador da tabela pedidos. Ele também é uma chave estrangeira da tabela lojas.';
 
 -- Criação de tabela com o nome de lojas.pedidos_itens.
 CREATE TABLE 	pedidos_itens (
-  				pedido_id 			        NUMERIC(38) 	NOT NULL,
-  				produto_id 			        NUMERIC(38) 	NOT NULL,
-  				numero_da_linha 	        NUMERIC(38) 	NOT NULL,
-  				preco_unitario 		        NUMERIC(10, 2) 	NOT NULL,
-  				quantidade 			        NUMERIC(38) 	NOT NULL,
-  				envio_id 			        NUMERIC(38),
+                pedido_id 			        NUMERIC(38) 	NOT NULL,
+                produto_id 			        NUMERIC(38) 	NOT NULL,
+                numero_da_linha 	        NUMERIC(38) 	NOT NULL,
+                preco_unitario 		        NUMERIC(10, 2) 	NOT NULL,
+                quantidade 			        NUMERIC(38) 	NOT NULL,
+                envio_id 			        NUMERIC(38),
 CONSTRAINT      pk_pedidos_itens            PRIMARY KEY (pedido_id, produto_id)
 );
 
 -- Comentários para explicações de tabela e colunas de tabela lojas.pedidos_itens.
 ALTER TABLE     pedidos_itens               COMMENT 'A tabela pedidos_itens que tem por sua responsabilidade de verificar os pedidos de produto e é responsável para remeter essas informações à tabela envios.';
-ALTER TABLE     pedidos_itens               MODIFY COLUMN   pedido_id 		    NUMERIC(38)   	COMMENT 'Coluna pedido_id que é responsável como identificador da tabela pedidos_itens. Ele também é uma chave primária da tabela pedidos_itens e uma chave estrangeira da tabela pedidos.';
-ALTER TABLE     pedidos_itens               MODIFY COLUMN   produto_id 		    NUMERIC(38)   	COMMENT 'Coluna produto_id que é responsável como identificador da tabela pedidos_itens. Ele também é uma chave primária da tabela pedidos_itens e uma chave estrangeira da tabela produtos.';
+ALTER TABLE     pedidos_itens               MODIFY COLUMN   pedido_id           NUMERIC(38)   	COMMENT 'Coluna pedido_id que é responsável como identificador da tabela pedidos_itens. Ele também é uma chave primária da tabela pedidos_itens e uma chave estrangeira da tabela pedidos.';
+ALTER TABLE     pedidos_itens               MODIFY COLUMN   produto_id          NUMERIC(38)   	COMMENT 'Coluna produto_id que é responsável como identificador da tabela pedidos_itens. Ele também é uma chave primária da tabela pedidos_itens e uma chave estrangeira da tabela produtos.';
 ALTER TABLE     pedidos_itens               MODIFY COLUMN   numero_da_linha     NUMERIC(38)   	COMMENT 'Coluna numero_da_linha que é responsável por identificar sequencialmente o registro de cada item de pedido.';
-ALTER TABLE     pedidos_itens               MODIFY COLUMN   preco_unitario 	    NUMERIC(10, 2)	COMMENT 'Coluna preco_unitario que é responsável por mostrar o preço de unidade por cada item de pedido.';
+ALTER TABLE     pedidos_itens               MODIFY COLUMN   preco_unitario      NUMERIC(10, 2)	COMMENT 'Coluna preco_unitario que é responsável por mostrar o preço de unidade por cada item de pedido.';
 ALTER TABLE     pedidos_itens               MODIFY COLUMN   quantidade 	        NUMERIC(38)     COMMENT 'Coluna quantidade que é responsável por guardar os informações de quantidade dos pedidos de itens.';
 
 
