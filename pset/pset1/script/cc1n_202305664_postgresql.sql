800b351cc4adaf060745ef438ad5e814591b0beb
Erick Jun Kato <116921226+nomenome-cmd@users.noreply.github.com>
2023-05-31 15:04:41 -0300

    postgresql.sql

--- pset/pset1/script/cc1n_202305664_postgresql.sql
+++ pset/pset1/script/cc1n_202305664_postgresql.sql
@@ -182,7 +182,7 @@ CONSTRAINT 	pk_envios 					PRIMARY KEY 	(envio_id)
 --Checagem da tabela lojas.envios para restringir a inserção de dados da coluna status para ser: Cancelado, Completo, Aberto, Pago, Reembolsa, Enviado.
 ALTER TABLE  	lojas.envios
 ADD CONSTRAINT 	ck_status
-CHECK 		(status in ('CRIADO', 'ENVIADO', 'TRANSITO', 'ENTREGUE');
+CHECK 		(status in ('CRIADO', 'ENVIADO', 'TRANSITO', 'ENTREGUE'));
 
 --Comentários para explicações de tabela e colunas de tabela lojas.envios.
 COMMENT ON TABLE  lojas.envios					IS 'A tabela envios que tem por sua responsabilidade de guardar os dados de envios dos itens de pedidos para seus clientes e das lojas.';
@@ -211,7 +211,7 @@ CONSTRAINT 	pk_pedidos 					PRIMARY KEY 	(pedido_id)
 --Checagem da tabela lojas.pedidos para restringir a inserção de dados da coluna status para ser: Cancelado, Completo, Aberto, Pago, Reembolsa, Enviado.
 ALTER TABLE  	lojas.pedidos
 ADD CONSTRAINT 	ck_status
-CHECK 		(status in ('CANCELADO', 'COMPLETO', 'ABERTO', 'PAGO', 'REEMBOLSADO', 'ENVIADO');
+CHECK 		(status in ('CANCELADO', 'COMPLETO', 'ABERTO', 'PAGO', 'REEMBOLSADO', 'ENVIADO'));
 
 --Comentários para explicações de tabela e colunas de tabela lojas.pedidos.
 COMMENT ON TABLE  lojas.pedidos					IS 'A tabela pedidos que tem por sua responsabilidade de guardar pedidos dos seus clientes à loja.';
